1ae8a66bc48406848e68749b05b315f875047865
ilia <iliavabez@gmail.com>
2018-03-23 15:14:25 +0300

    index -1 bug fix

--- pick_a_card.rb
+++ pick_a_card.rb
@@ -2,9 +2,8 @@
 values = ['2','3','4','5','6','7','8','9','10','J','Q','K','A']
 suits = ['♣', '♥', '♦', '♠'] # Буби, Черви, Крести, Пики
 
-<<<<<<< HEAD
 puts "Pick a card 2018"
-=======
+
 cards = []
 
 values.each do |value|
@@ -16,12 +15,8 @@ end
 cards.shuffle!
 
 puts "How many cards do you pick?"
-number = STDIN.gets.to_i - 1
+number = STDIN.gets.to_i
 
 number.times do
   puts cards.pop
 end
->>>>>>> multiple_cards
-
-# Выведем произвольную карты, выбрав по одному элементу из массивов
-puts "#{values.sample} of #{suits.sample.encode('utf-8')}"
